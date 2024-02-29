d298ad030b86ee3728aed179c193042c6fa9ff28
Asad-Ismail <asadismaeel@gmail.com>
2024-02-29 23:07:06 +0000

    updated data preprocessor

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -24,8 +24,9 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--user-features', type=str, default='customers.csv', help='Filename of user features data.')
     parser.add_argument('--feature-cols', type=str, default='', help='Comma-separated feature columns in the data.')
     parser.add_argument('--label-col', type=str, default='credit_application', help='Name of the label column in the data.')
-    parser.add_argument('--train-data-ratio', type=float, default=0.8, help='Fraction of data to use for the training set.')
-    parser.add_argument('--test-data-ratio', type=float, default=0.1, help='Fraction of data to use for the test set.')
+    parser.add_argument('--train-data-ratio', type=float, default=0.9, help='Fraction of data to use for the training set.')
+    parser.add_argument('--val-data-ratio', type=float, default=0.1, help='Fraction of data to use for the val set.')
+    parser.add_argument('--time_based_split', type=bool, default=True, help='Use Time based splitting for dataset')
     return parser.parse_args()
 
 def get_logger(name: str) -> logging.Logger:
@@ -58,7 +59,7 @@ def apply_log_transform(df: pd.DataFrame) -> pd.DataFrame:
     df[numeric_cols] = df[numeric_cols].apply(lambda x: np.log1p(x))
     return df
 
-def apply_standard_scaling(train_df: pd.DataFrame, val_df: pd.DataFrame, test_df: pd.DataFrame, feature_cols: list) -> tuple:
+def apply_standard_scaling(train_df: pd.DataFrame, val_df: pd.DataFrame, feature_cols: list) -> tuple:
     """
     Fits a standard scaler on the training data and applies the transformation to the training, validation, and test datasets.
     
@@ -74,14 +75,12 @@ def apply_standard_scaling(train_df: pd.DataFrame, val_df: pd.DataFrame, test_df
     scaler = StandardScaler()
     train_df_scaled = scaler.fit_transform(train_df[feature_cols])
     val_df_scaled = scaler.transform(val_df[feature_cols])
-    test_df_scaled = scaler.transform(test_df[feature_cols])
 
     # Update the original dataframes with the scaled values
     train_df[feature_cols] = train_df_scaled
     val_df[feature_cols] = val_df_scaled
-    test_df[feature_cols] = test_df_scaled
 
-    return train_df, val_df, test_df
+    return train_df, val_df
 
 def plot_distributions(df: pd.DataFrame, columns: list, output_dir: str, prefix: str = "") -> None:
     """
@@ -102,7 +101,7 @@ def plot_distributions(df: pd.DataFrame, columns: list, output_dir: str, prefix:
         plt.savefig(os.path.join(output_dir, f"{prefix}{column}_distribution.png"))
         plt.close()
 
-def preprocess_data(train_df: pd.DataFrame, val_df: pd.DataFrame, test_df: pd.DataFrame, output_dir: str, apply_log: bool = False, apply_scaling: bool = False) -> None:
+def preprocess_data(train_df: pd.DataFrame, val_df: pd.DataFrame, output_dir: str, apply_log: bool = False, apply_scaling: bool = False, apply_imputer: bool = True) -> tuple:
     """
     Preprocesses the data by applying log transformation and standard scaling based on flags.
     Plots the feature distributions before and after transformations.
@@ -110,11 +109,12 @@ def preprocess_data(train_df: pd.DataFrame, val_df: pd.DataFrame, test_df: pd.Da
     Args:
         train_df (pd.DataFrame): Training data DataFrame.
         val_df (pd.DataFrame): Validation data DataFrame.
-        test_df (pd.DataFrame): Testing data DataFrame.
         output_dir (str): Directory to save processed files and plots.
         apply_log (bool): Flag to apply log transformation.
         apply_scaling (bool): Flag to apply standard scaling.
     """
+    train_df=train_df.copy()
+    val_df= val_df.copy()
     # Ensure output directory exists
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
@@ -129,15 +129,22 @@ def preprocess_data(train_df: pd.DataFrame, val_df: pd.DataFrame, test_df: pd.Da
         # Apply log transformation
         train_df = apply_log_transform(train_df)
         val_df = apply_log_transform(val_df)
-        test_df = apply_log_transform(test_df)
         # Plot distributions after log transformation
         plot_distributions(train_df, feature_cols, output_dir, "log_")
     
     if apply_scaling:
         # Apply standard scaling correctly across datasets
-        train_df, val_df, test_df = apply_standard_scaling(train_df, val_df, test_df, feature_cols)
+        train_df, val_df= apply_standard_scaling(train_df, val_df, feature_cols)
         # Plot distributions after standard scaling
         plot_distributions(train_df, feature_cols, output_dir, "scaled_")
+    
+    if apply_imputer:
+        # Impute missing 'CRG' values with the median
+        crg_imputer = train_df['CRG'].median()
+        for df in [train_df, val_df]:
+            df['CRG'].fillna(crg_imputer, inplace=True)
+
+    return  train_df,val_df
 
 
 
@@ -168,7 +175,7 @@ def load_data(data_dir: str, credit_data: str, features_data: str, label_col: st
 
     return merged_df
 
-def split_data(df: pd.DataFrame, label_col: str, train_data_ratio: float, test_data_ratio: float) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def split_data_stratified(df: pd.DataFrame, label_col: str, train_data_ratio: float, val_data_ratio: float) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
     Splits data into train, validation, and test sets based on specified ratios.
 
@@ -181,27 +188,70 @@ def split_data(df: pd.DataFrame, label_col: str, train_data_ratio: float, test_d
     Returns:
         Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]: The train, validation, and test DataFrames.
     """
-    val_data_ratio = 1 - train_data_ratio - test_data_ratio
-    logging.info(f"Data split ratios - Train: {train_data_ratio}, Validation: {val_data_ratio}, Test: {test_data_ratio}")
+    logging.info(f"Data split ratios - Train: {train_data_ratio}, Validation: {val_data_ratio}")
 
     # Stratified sampling based on 'client_nr' and 'credit_application'
     client_label_dist = df.groupby('client_nr')[label_col].mean().reset_index()
     label_dist = df[label_col].mean()
     client_label_dist['stratum'] = (client_label_dist[label_col] > label_dist).astype(int)
 
-    client_tv, client_test = train_test_split(client_label_dist, test_size=test_data_ratio, stratify=client_label_dist['stratum'], random_state=42)
-    client_train, client_val = train_test_split(client_tv, test_size=val_data_ratio / (1 - test_data_ratio), stratify=client_tv['stratum'], random_state=42)
+    client_train, client_val = train_test_split(client_label_dist, test_size=val_data_ratio, stratify=client_label_dist['stratum'], random_state=42)
 
     train_df = df[df['client_nr'].isin(client_train['client_nr'])]
     val_df = df[df['client_nr'].isin(client_val['client_nr'])]
-    test_df = df[df['client_nr'].isin(client_test['client_nr'])]
 
-    logging.info(f"Data percentages - Train: {len(train_df) / len(df) * 100:.2f}%, Validation: {len(val_df) / len(df) * 100:.2f}%, Test: {len(test_df) / len(df) * 100:.2f}%")
-    logging.info(f"Credit application percentages - Train: {train_df[label_col].mean() * 100:.2f}%, Validation: {val_df[label_col].mean() * 100:.2f}%, Test: {test_df[label_col].mean() * 100:.2f}%")
+    logging.info(f"Data percentages - Train: {len(train_df) / len(df) * 100:.2f}%, Validation: {len(val_df) / len(df) * 100:.2f}%")
+    logging.info(f"Credit application percentages - Train: {train_df[label_col].mean() * 100:.2f}%, Validation: {val_df[label_col].mean() * 100:.2f}%")
 
-    return train_df, val_df, test_df
+    return train_df, val_df
 
-def get_features_and_labels(train_df: pd.DataFrame, val_df: pd.DataFrame, test_df: pd.DataFrame, output_dir: str) -> None:
+
+def split_data_time(df: pd.DataFrame, label_col: str, n: int = 3) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Splits data into train, validation, and test sets based on specified ratios.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to split.
+        label_col (str): The name of the label column.
+        n (int): n month data for each user is used as test set.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]: The train, validation, and test DataFrames.
+    """
+    logging.info(f"Data split ratios - Train: {train_data_ratio}, Validation: {val_data_ratio}")
+
+    df_sorted= df.sort_values(by=['client_nr', 'yearmonth'], ascending=[True, True])
+    # Get the last entry for each 'client_nr' as the test set
+    val_df = df_sorted.groupby('client_nr').tail(n)
+    
+    # Use the remaining data as the training set
+    train_df = df_sorted.drop(test_df.index)
+
+    logging.info(f"Data percentages - Train: {len(train_df) / len(df) * 100:.2f}%, Validation: {len(val_df) / len(df) * 100:.2f}%")
+    logging.info(f"Credit application percentages - Train: {train_df[label_col].mean() * 100:.2f}%, Validation: {val_df[label_col].mean() * 100:.2f}%")
+
+    return train_df, val_df
+
+
+def split_data(df: pd.DataFrame, label_col: str, train_data_ratio: float, val_data_ratio: float, time_split: bool= True) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Splits data into train, validation, and test sets based on specified ratios.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to split.
+        label_col (str): The name of the label column.
+        train_data_ratio (float): The ratio of data to use for training.
+        test_data_ratio (float): The ratio of data to use for testing.
+
+    Returns:
+        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]: The train, validation, and test DataFrames.
+    """
+    if time_split:
+        return split_data_time(df,label_col,n=3)
+    else:
+        return split_data_stratified(df,label_col,train_data_ratio,val_data_ratio)
+
+def save_processed_data(train_df: pd.DataFrame, val_df: pd.DataFrame, output_dir: str) -> None:
     """
     Processes the data, imputes missing values, and saves features and labels for training, validation, and testing.
 
@@ -211,21 +261,15 @@ def get_features_and_labels(train_df: pd.DataFrame, val_df: pd.DataFrame, test_d
         test_df (pd.DataFrame): Testing data DataFrame.
         output_dir (str): Directory to save the processed files.
     """
-    # Impute missing 'CRG' values with the median
-    crg_imputer = train_df['CRG'].median()
-    for df in [train_df, val_df, test_df]:
-        df['CRG'].fillna(crg_imputer, inplace=True)
 
     # Split features and labels
     X_train, y_train = train_df.drop(['client_nr', 'yearmonth', 'credit_application', 'nr_credit_applications'], axis=1), train_df['credit_application']
     X_val, y_val = val_df.drop(['client_nr', 'yearmonth', 'credit_application', 'nr_credit_applications'], axis=1), val_df['credit_application']
-    X_test, y_test = test_df.drop(['client_nr', 'yearmonth', 'credit_application', 'nr_credit_applications'], axis=1), test_df['credit_application']
 
     # Save to CSV
     file_paths = {
         'train_features.csv': X_train, 'train_labels.csv': y_train,
         'val_features.csv': X_val, 'val_labels.csv': y_val,
-        'test_features.csv': X_test, 'test_labels.csv': y_test
     }
     for file_name, data in file_paths.items():
         data.to_csv(os.path.join(output_dir, file_name), index=False)
@@ -236,5 +280,6 @@ if __name__ == '__main__':
     args = parse_args()
 
     df = load_data(args.data_dir, args.credit_applications, args.user_features, args.label_col)
-    train_df, val_df, test_df = split_data(df, args.label_col, args.train_data_ratio, args.test_data_ratio)
-    get_features_and_labels(train_df, val_df, test_df, args.output_dir)
+    train_df, val_df = split_data_stratified(df, args.label_col, args.train_data_ratio,args.val_data_ratio)
+    train_df, val_df = preprocess_data(train_df, val_df, args.output_dir,apply_log=True, apply_scaling=True, apply_imputer=True)
+    save_processed_data(train_df, val_df, args.output_dir)
